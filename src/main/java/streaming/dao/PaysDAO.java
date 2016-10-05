9a82c8e26b0d521f952e288f8c56957d28581edf
ellawy633 <abd.alay@hotmail.com>
2016-10-05 10:33:29 +0200

    zert

---
+++ src/main/java/streaming/dao/PaysDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import streaming.entity.Pays;
+
+/**
+ *
+ * @author admin
+ */
+@Repository
+public class PaysDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public void ajouter(Pays pays) {
+        em.persist(pays);
+
+    }
+
+    public Pays rechercher(long id) {
+
+        return em.find(Pays.class, id);
+    }
+
+}
---
+++ src/test/java/streaming/test/PersonneDAOTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import streaming.dao.PersonneDAO;
+import streaming.entity.Personne;
+import streaming.spring.SpringConfig;
+
+/**
+ *
+ * @author admin
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringConfig.class)
+public class PersonneDAOTest {
+
+    @Autowired
+    private PersonneDAO dao;
+
+    @Test
+    public void test1() {
+
+        Personne per = new Personne();
+        per.setNom("FRANCSOIS");
+        dao.ajouter(per);
+    }
+
+    @Test
+    public void test2() {
+
+        dao.listertous();
+    }
+}
